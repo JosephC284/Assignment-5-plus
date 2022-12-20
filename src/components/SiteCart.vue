ff9595debd7b1083abaae0820f8d99f3721b6bb6
[JosephC282] <jchang284@gmail.com>
2022-12-20 14:57:23 -0500

    Testing.

--- src/components/SiteCart.vue
+++ src/components/SiteCart.vue
@@ -1,19 +1,26 @@
 <script setup>
+import { remove } from "@vue/shared";
 import { useStore } from "../store/index.js";
 
 const store = useStore();
 console.log()
 
+let remove = (index) => {
+  delete store.cart[index]
+  console.log(index);
+}
+
+
 </script>
 
 <template>
   <div class="box">
-    <p v-for="movie in Array.from(store.cart.values())">Movie Title: {{ movie.title }}</p>
-    <img
-      v-for="movie in Array.from(store.cart.values())"
-      :src="`https://image.tmdb.org/t/p/w500${movie.poster}`"
-    />
+    <p v-for="movie of store.cart">Movie Title: {{ movie.title }}</p>
+    <img v-for="movie of store.cart" :src= "`https://image.tmdb.org/t/p/w500${movie.poster}`" 
+    @click = remove(movie.id) />
   </div>
+
+  <h1>{{store.cart}}</h1>
 </template>
 
 <style scoped>
@@ -22,8 +29,14 @@ console.log()
   grid-template-columns: repeat(2, 1fr);
 }
 
+p {
+  grid-column-start: 1;
+  grid-column-end: 1;
+}
 img {
   width: 200px;
   height: 300px;
+  grid-column-start: 2;
+  grid-column-end: 2;
 }
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,32 +1,38 @@
 import { defineStore } from "pinia";
 import axios from "axios";
-import SiteBuy from '../components/SiteBuy.vue';
+import SiteBuy from "../components/SiteBuy.vue";
 
 export const useStore = defineStore("store", {
   state: () => ({
-      movies: [],
-      cart: new Map(),
+    movies: {},
+    cart: {},
   }),
   actions: {
     async getMovies() {
-      let data = (
-        await axios.get("https://api.themoviedb.org/3/trending/movie/week", {
+      let data = await axios.get(
+        "https://api.themoviedb.org/3/trending/movie/week",
+        {
           params: {
             api_key: "ad891932e4907c23fc8a99002a4b5d3f",
             include_adult: "false",
           },
-        })
-      )
-      for(let movie of data.data.results){
-      this.movies.push({
-        id: movie.id,
-        poster: "https://image.tmdb.org/t/p/w500" + movie.poster_path,
-      });
-    }
+        }
+      );
+      for (let movie of data.data.results) {
+        this.movies = {
+          id: movie.id,
+          poster: "https://image.tmdb.org/t/p/w500" + movie.poster_path,
+        };
+      }
     },
-  addToCart(id, data) {
-    this.cart.set(id,data)
-  }
+    addToCart(id, data) {
+      if (!this.cart[id]) {
+        this.cart[id] = {
+          id: id,
+          data: data,
+        };
+      }
+      console.log(this.cart);
+    },
   },
 });
- 
